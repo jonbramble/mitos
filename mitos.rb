0275a6837e7333e0980c510374acf799ccca9b97
Jonathan Bramble <jbramble82@gmail.com>
2015-12-02 18:31:59 +0000

    fixed up correct commands to send and receive

--- mitos.rb
+++ mitos.rb
@@ -6,60 +6,67 @@ class Mitos
 		@sp = SerialPort.new(port,9600,8,1)
 
  		flush_coms
-		#init_syringes
+		init_syringes
 		status
 	end
 
 	def status
-        @sp.write("$01S3\n")
+
+        @sp.write("$00S3\r")
         sleep(0.25)
-        while data = @sp.readline
-        	puts data
-        end
-        #@sp.write("$01S3\n")
-        #sleep(0.25)
-        #puts @sp.readline
-        #@sp.flush_input
+  
+        puts @sp.read(15)
+
+        @sp.write("$01S3\r")
+        sleep(0.25)
+
+        puts @sp.read(15)
+		sleep(1)
+
+		puts("port")
+		
+
+        @sp.write("$01E2 2 16\r")
+        puts @sp.read(7)
+
 	end
-
-	def run
-   	 puts "Listening on serial port #{@portname}"
-    
-   	 @sp.flush_input
-     begin 
- 		while data = @sp.readline
- 			#parse_input(data)
- 			puts data
- 		end
- 	    rescue Interrupt
-  		 puts "exiting"	
-  	end
-
-   end
 
 private
 
    	def flush_coms
-		@sp.write("$00F\n")
+		@sp.write("$00F\r")
 		sleep(0.25)
-		@sp.write("$01F\n")
+		@sp.write("$01F\r")
 		sleep(0.25)
-		@sp.flush_input
+		#@sp.flush_input
 	end
 
 	def init_syringes
+		sleep(1)
 		puts "Initialising pump valves"
-		@sp.write("$00I2\r\n")
+		@sp.write("$00I1\r")
 		sleep(0.25)
-		@sp.write("$01I2\r\n")
+		puts @sp.read(7)
+
+		@sp.write("$01I2\r")
 		sleep(0.25)
+		puts @sp.read(7)
+		
 		sleep(2)
 		puts "Initialising syringes"
-		@sp.write("$00I1\r\n")
+		@sp.write("$00I1\r")
 		sleep(0.25)
-		@sp.write("$01I1\r\n")
+		puts @sp.read(7)
+		
+		@sp.write("$01I1\r")
 		sleep(0.25)
+		puts @sp.read(7)
+
+		
+		
+		#sleep(0.25)
 		sleep(2)
+		@sp.flush_input
 	end
 
 end
