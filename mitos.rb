d2a32c78c2c6bfbe5195653646c4e915a35c6af6
Jonathan Bramble <jbramble82@gmail.com>
2015-12-03 11:12:04 +0000

    added port commands

--- mitos.rb
+++ mitos.rb
@@ -15,8 +15,12 @@ module Mitos
       MOVE_SYRINGE_POS = "E2 1"
       MOVE_VALVE_POS = "E2 2"
 
-      MYSTERY_V = "V"
+      FOUR_PORT_A = "0"
+      FOUR_PORT_B = "6"
+      FOUR_PORT_C = "12"
+      FOUR_PORT_D = "18"
 
+      MYSTERY_V = "V"
 
 
 	 def initialize(port)
@@ -27,24 +31,43 @@ module Mitos
 	 end
 
 	 def status
+	 	#might have a problem if the number of bytes varies
 	 	read_15 = read_bytes(15)
+        pump_command(0,STATUS,read_15)
+        pump_command(1,STATUS,read_15)
+	 end
 
-        write(0,STATUS)
-        puts read_15.call
-
-        write(1,STATUS)
-        str = read_15.call
-
-        # parse inputs
-
-        # check the input is of the correct format - regexp
-        # split the string on spaces
-
-        puts str.split(" ")
+	 def set_port(address,position)
 
 	 end
 
 	private
+
+	 def parse_response(str)
+	 	# check the input is of the correct format - regexp
+	 	puts str
+	 	res = str.split(" ")
+	 	header = res[0].split("")
+	 	type = header[3].to_i
+
+	 	#for testing
+	 	
+	 	#command recieved
+	 	if type==1
+	 		puts "Command"
+	 		puts res[1]
+	 		## make some kind of response hash
+	 	#status
+	 	elsif type==9
+	 		puts "Status"
+	 		puts res[1]
+	 	elsif type==8
+	 		puts "Error, restart program"
+	 	else
+	 		puts "Unknown response"
+	 	end
+
+	 end
 
    	 def flush_coms
 		write(0,FLUSH)
@@ -53,28 +76,24 @@ module Mitos
 	 end
 
 	 def init_syringes
-
 	 	read_7 = read_bytes(7)
 
 		puts "Initialising pump valves"
-
-		write(0,INITIALIZE_VALVE)
-		puts read_7.call
-
-		write(1,INITIALIZE_VALVE)
-		puts read_7.call
+		pump_command(0,INITIALIZE_VALVE,read_7)
+		pump_command(1,INITIALIZE_VALVE,read_7)
 		
 		sleep(2)
 
 		puts "Initialising syringes"
-
-		write(0,INITIALIZE_SYRINGE)
-		puts read_7.call
-		
-		write(1,INITIALIZE_SYRINGE)
-		puts read_7.call
+		pump_command(0,INITIALIZE_SYRINGE,read_7)
+		pump_command(1,INITIALIZE_SYRINGE,read_7)
 
 		flush_input
+	 end
+
+	 def pump_command(address,request,read_proc)
+	 	write(address,request)
+	 	parse_response(read_proc.call)
 	 end
 
 	 def flush_input
@@ -98,3 +117,6 @@ end
 
 pump = Mitos::XsDuoBasic.new("COM1")
 pump.status
+
+#low level valve command
+pump.set_port(0,"A")
